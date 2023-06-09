772091c155cc4ab064abaa9ff1886d2ce709d1c5
Geoffrey-Jiang <88568399+Geoffrey-Jiang@users.noreply.github.com>
2023-06-09 12:29:00 +1000

    Added colour options

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Detect if mouse is being pressed - used for colouring
 let isDown = false;
 const body = document.querySelector("body");
 body.addEventListener("mousedown", () => {
@@ -8,6 +9,40 @@ body.addEventListener("mouseup", () => {
     isDown = false;
 })
 
+// Button controls
+let size = 16;
+createGrid(size);
+
+// Size - Resize using createGrid function and detect click and prompt user
+document.getElementById("size").onclick = function () {
+    size = prompt("What would you like the new size to be?");
+    if (size >= 1 && size <= 100) {
+        createGrid(size);
+    }
+}
+
+// Reset - Reset canvas by creating fresh one
+document.getElementById("reset").onclick = function () {
+    createGrid(size);
+}
+
+// Colours
+const BLACK = 0;
+const SHADE = 1;
+const RAINBOW = 2;
+let colour = BLACK;
+document.getElementById("black").onclick = function () {
+    colour = BLACK;
+}
+document.getElementById("shading").onclick = function () {
+    colour = SHADE;
+}
+document.getElementById("rainbow").onclick = function () {
+    colour = RAINBOW;
+}
+
+
+// Creates a grid of div's and adds event listeners for functionality
 function createGrid(size) {
     const grid = document.querySelector('#grid_square');
     // Remove previous grid
@@ -27,6 +62,7 @@ function createGrid(size) {
             // Create elements to go into the columns
             const grid_col = document.createElement('div');
             grid_col.classList.toggle('grid');
+            grid_col.style['background-colour'] = "rgba(0, 0, 0, 0)"
             grid_row.appendChild(grid_col);
 
             // Create event listeners for hovering effect or click
@@ -35,7 +71,8 @@ function createGrid(size) {
                 if (isDown === false) {
                     e.target.classList.add('hover');
                 } else {
-                    e.target.classList.add('add_color');
+                    // Depends on selection
+                    adjustColour(e);
                 }
             })
 
@@ -47,18 +84,22 @@ function createGrid(size) {
     }
 }
 
-let size = 16;
-createGrid(size);
-
-// Resize using createGrid function and detect click and prompt user
-document.getElementById("size").onclick = function () {
-    size = prompt("What would you like the new size to be?");
-    if (size >= 1 && size <= 100) {
-        createGrid(size);
+// Adjusts colour based on colour selected
+function adjustColour (e) {
+    if (colour === BLACK) {
+        e.target.style['background-color'] = "rgba(0, 0, 0, 1)"
+    } else if (colour === SHADE) {
+        const colour = e.target.style['background-color'];
+        // Either extracts " 0" or ".x" and converts it to a decimal successfully
+        let alpha_value = Number(colour.slice(colour.length - 3, colour.length - 1));
+        if (alpha_value !== 1) {
+            e.target.style['background-color'] = `rgba(0, 0, 0, ${alpha_value + 0.1})`;
+        }
+    } else if (colour === RAINBOW) {
+        e.target.style['background-color'] = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`
     }
 }
 
-// Reset canvas by creating fresh one
-document.getElementById("reset").onclick = function () {
-    createGrid(size);
+function randomNum () {
+    return Math.floor(Math.random() * 256);
 }
